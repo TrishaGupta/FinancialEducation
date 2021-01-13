dcafc17529e4dca99cbf2bd05bec9bde98dd79b9
TrishaGupta <trishangupta@gmail.com>
2021-01-13 12:22:29 +0530

    Add files via upload

--- xiir.py
+++ xiir.py
@@ -14,19 +14,15 @@ start_date="31-10-2017"
 end_date_temp="#"
 end_date_temp+= str(date.today())
 end_date_temp=str(end_date_temp)
-end_date=""
+end_date="" 
 temp=""
 
 
 #flip the date to dd/mm/yyyy
 for num in range(len(end_date_temp)-1,-1,-1):
-	#print ("1")
-	#print (num)
 	if end_date_temp[num] is not '-' and end_date_temp[num] is not '#' :
-		#print ("2")
 		temp+=end_date_temp[num]
 	else:
-		#print ("3")
 		rev_temp=""
 		for num2 in range(len(temp)-1,-1,-1):
 			rev_temp+= temp[num2]
@@ -34,15 +30,53 @@ for num in range(len(end_date_temp)-1,-1,-1):
 		if num>1:
 			end_date+="-"
 		temp=""
-			
-url = "https://www.quandl.com/api/v3/datasets/BSE/"+symbol+"/data.json?api_key=DJS3s5-qSxQRxf4KCwjW&qtops.columns=Close&start_date="+ start_date + "&end_date=" + end_date
+
+url = "https://www.quandl.com/api/v3/datasets/BSE/"+symbol+"/data.json?api_key=DJS3s5-qSxQRxf4KCwjW&start_date="+ start_date + "&end_date=" + end_date+ "&column_index=4"
 #start_date="+ start_date + "&end_date=" + end_date + "api_key=DJS3s5-qSxQRxf4KCwjW"
+
 
 
 
 response = requests.request("GET", url)
 todos=json.loads(response.text)
-print(todos)
+todos_dataset= todos['dataset_data']
+todos_data=todos_dataset['data']
+
+x = np.arange(0,len(todos_data),1)
+xTicks = [] #dates
+y_close = [] #closing values
+
+#[date,value]
+for i in range(0,len(todos_data)):
+	xTicks.append(todos_data[i][0])
+	y_close.append(todos_data[i][1])
+	y_close_sma10=[]
+#calculate 10 day sma; we take the average of 10 days from y_close
+#temp=1
+#avg=0
+
+#for i in range(0,len(todos_data)):
+
+
+
+
+
+#plot graph here
+plt.xticks(x, xTicks)
+plt.xticks(range(int(len(todos_data))), xTicks, rotation=90, fontsize=1) #writes strings with 45 degree angle
+plt.plot(x,y_close,'-ok')
+plt.show()
+
+
+
+
+#print(json.dumps(todos_data,indent=4,sort_keys=True))
+
+#for x in range(5):
+	#for y in todos_data[x]:
+		#print (y)
+
+#print(todos)
 
 #todos_dataset=todos['dataset_data']
 #print(todos_dataset[''])
